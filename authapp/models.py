a526b36d8b2ad1cf75cc45ea8cdc05a16c2f74dd
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-21 14:27:46 +0300

    add new

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length=100)
+    profile=models.ImageField(upload_to='images/', blank=True)
+    email = models.CharField(blank = True, max_length = 100)
+    bio = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+    def save_user(self):
+        self.save()
+        
+    @classmethod
+    def get_user(cls,id):
+        profile = Profile.objects.all()
+        return profile
+
+    @classmethod
+    def update_user(cls,id,new_username):
+        cls.objects.filter(id=id).update(username = new_username)
+
+    @classmethod
+    def delete_user(cls,id):
+        cls.objects.filter(id).delete()
+
+
+    @classmethod
+    def search_by_username(cls, searched_username):
+        username = cls.objects.filter(username__icontains=searched_username)
+
+        return username
